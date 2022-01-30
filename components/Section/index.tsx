4f79b0f18476a8fbc1ce5f6aa6524f76d80b78de
Avvy <saberwatch@gmail.com>
2022-01-29 21:33:48 -0800

    initial commit

---
+++ components/Section/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+const Section = () => {
+  return (
+    <div className={styles.container}>
+      <>p</>
+    </div>
+  );
+};
+
+export default Section;
